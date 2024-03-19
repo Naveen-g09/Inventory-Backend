b8213e3f7b2b34d4fa711f4a38c07fe6c566a214
Naveen <naveengummella.rg@gmail.com>
2024-03-19 21:38:11 +0530

    database integration to server

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import pg from 'pg';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const PORT = 3000;
 
 const db = new pg.Client({
     user: 'postgres',
@@ -9,15 +17,36 @@ const db = new pg.Client({
 });
 
 db.connect()
-
-db.query('SELECT * FROM medicine', (err, res) => {
-    if (err) {
+    .then(() => {
+        console.log('Connected to the database successfully')
+    })
+    .catch(err => {
         console.log(err.stack)
-    } else {
-        console.log(res.rows)
-    }
+    })
 
-    db.end()
+
+app.get('/get-medicine', (req, res) => {
+    db.query('SELECT * FROM medicine', (err, result) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            res.send(result.rows)
+        }
+    })
 })
 
+app.post('/post-medicine', (req, res) => {
+    const { name, quantity } = req.body;
+    db.query('INSERT INTO medicine (name, quantity) VALUES ($1, $2)', [name, quantity], (err, result) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            res.send('Data added successfully')
+        }
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
 
